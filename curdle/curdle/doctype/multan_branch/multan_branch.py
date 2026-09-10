d002cdaea279f344edff5b9f3c0afe0ef53db824
SafdarAliGit <safdar211@gmail.com>
2026-09-10 18:38:13 +0500

    updated

--- curdle/curdle/doctype/multan_branch/multan_branch.py
+++ curdle/curdle/doctype/multan_branch/multan_branch.py
@@ -45,9 +45,9 @@ class MultanBranch(Document):
         payment.mode_of_payment = self.mop
         payment.amount = float(self.grand_total)
         if self.mop == 'Cash':
-            posi.taxes_and_charges = 'GST on Cash - CM'
+            posi.taxes_and_charges = 'GST on Cash - C'
         elif self.mop == 'Credit Card':
-            posi.taxes_and_charges = 'GST on Credit Card - CM'
+            posi.taxes_and_charges = 'GST on Credit Card - C'
 
         try:
             posi.submit()
